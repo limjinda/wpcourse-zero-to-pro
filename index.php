e019b682923365488e8e3f3bebd6a936ee88954e
wpcourse <wpcourse@PPPPPPPs-MBP.local>
2021-06-08 21:35:29 +0700

    first init

---
+++ index.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<!-- header -->
+			<header class="header">
+				<div class="header-left">
+					<a href="#" class="site-logo">
+						<img src="https://via.placeholder.com/80x80" alt="">
+					</a>
+					<ul class="site-navigation">
+						<li>Homepage</li>
+						<li>Menus</li>
+						<li>Promotions</li>
+						<li>Contact us</li>
+					</ul>
+				</div>
+				<div class="header-right">
+					<a href="#">Find a Store</a>
+				</div>
+			</header>
+			<!-- slider  -->
+			<section class="slider-block">
+				<img src="https://via.placeholder.com/1440x550" alt="" />
+			</section>
+			<!-- feature -->
+			<section class="feature-block">
+				<h3>ENJOY SUMMER FLAVORS</h3>
+				<div class="row">
+					<!-- feature left -->
+					<div class="col-6">
+						<a href="#">
+							<img src="https://via.placeholder.com/675" alt="" />
+						</a>
+					</div>
+					<!-- feature right -->
+					<div class="col-6">
+						<div class="row">
+							<!-- small feature -->
+							<div class="col-6">
+								<a href="#">
+									<img src="https://via.placeholder.com/322" alt="" />
+								</a>
+							</div>
+							<!-- small feature -->
+							<div class="col-6">
+								<a href="#">
+									<img src="https://via.placeholder.com/322" alt="" />
+								</a>
+							</div>
+							<!-- small feature -->
+							<div class="col-6">
+								<a href="#">
+									<img src="https://via.placeholder.com/322" alt="" />
+								</a>
+							</div>
+							<!-- small feature -->
+							<div class="col-6">
+								<a href="#">
+									<img src="https://via.placeholder.com/322" alt="" />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="feature-banner">
+					<a href="#">
+						<img src="https://via.placeholder.com/1380x350" alt="" />
+					</a>
+				</div>
+			</section>
+			<!-- footer -->
+			<footer class="footer">
+				<div class="sitemap">
+					<div class="row">
+						<!-- footer 1 -->
+						<div class="col-3">
+							<h4>About Starbugs</h4>
+							<ul class="sitemap-link">
+								<li>Our Heritage</li>
+								<li>Our Company</li>
+								<li>Responsibility</li>
+								<li>Career Center</li>
+							</ul>
+						</div>
+						<!-- footer 2 -->
+						<div class="col-3">
+							<h4>Drink at Home</h4>
+							<ul class="sitemap-link">
+								<li>Coffee Story</li>
+								<li>Coffee & Tea at Home</li>
+								<li>How to Brew</li>
+							</ul>
+						</div>
+						<!-- footer 3 -->
+						<div class="col-3">
+							<ul class="sitemap-link">
+								<li>Starbugs rewards</li>
+								<li>Membership</li>
+								<li>More Information</li>
+								<li>Help Center</li>
+							</ul>
+						</div>
+						<!-- footer 4 -->
+						<div class="col-3">
+							<ul class="footer-social-links">
+								<li>
+									<a href="#">
+										<img src="https://via.placeholder.com/48" alt="facebook" />
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="https://via.placeholder.com/48" alt="twitter" />
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<img src="https://via.placeholder.com/48" alt="line" />
+									</a>
+								</li>
+							</ul>
+							<p>This is a demo website that built for WordPress theme development, Zero to Pro course.</p>
+						</div>
+					</div>
+				</div>
+				<div class="copyright">
+					<div class="row">
+						<!-- copyright 1 -->
+						<div class="col-6">
+							<ul class="copyright-link">
+								<li><a href="#">Homepage</a></li>
+								<li><a href="#">Menus</a></li>
+								<li><a href="#">Promotions</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
+						</div>
+						<!-- copyright 2 -->
+						<div class="col-6">
+							<p>Starbugs.com | WordPress Theme for Zero to Pro Course.</p>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
---
+++ js/complied.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=complied.js.map
