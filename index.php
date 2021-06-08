19f991db09f58e7719a4fddd68b07a137fc38af6
wpcourse <wpcourse@PPPPPPPs-MBP.local>
2021-06-09 01:21:57 +0700

    finished css homepage

--- index.php
+++ index.php
@@ -12,140 +12,148 @@
 		<div class="wrapper">
 			<!-- header -->
 			<header class="header">
-				<div class="header-left">
-					<a href="#" class="site-logo">
-						<img src="https://via.placeholder.com/80x80" alt="">
-					</a>
-					<ul class="site-navigation">
-						<li>Homepage</li>
-						<li>Menus</li>
-						<li>Promotions</li>
-						<li>Contact us</li>
-					</ul>
-				</div>
-				<div class="header-right">
-					<a href="#">Find a Store</a>
+				<div class="container">
+					<div class="header-left">
+						<a href="#" class="site-logo">
+							<img src="https://via.placeholder.com/80x80" alt="">
+						</a>
+						<ul class="site-navigation">
+							<li><a href="#">Homepage</a></li>
+							<li><a href="#">Menus</a></li>
+							<li><a href="#">Promotions</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+					<div class="header-right">
+						<a href="#">Find a Store</a>
+					</div>
 				</div>
 			</header>
-			<!-- slider  -->
+			<!-- slider -->
 			<section class="slider-block">
 				<img src="https://via.placeholder.com/1440x550" alt="" />
 			</section>
 			<!-- feature -->
 			<section class="feature-block">
-				<h3>ENJOY SUMMER FLAVORS</h3>
-				<div class="row">
-					<!-- feature left -->
-					<div class="col-6">
-						<a href="#">
-							<img src="https://via.placeholder.com/675" alt="" />
-						</a>
-					</div>
-					<!-- feature right -->
-					<div class="col-6">
-						<div class="row">
-							<!-- small feature -->
-							<div class="col-6">
-								<a href="#">
-									<img src="https://via.placeholder.com/322" alt="" />
-								</a>
-							</div>
-							<!-- small feature -->
-							<div class="col-6">
-								<a href="#">
-									<img src="https://via.placeholder.com/322" alt="" />
-								</a>
-							</div>
-							<!-- small feature -->
-							<div class="col-6">
-								<a href="#">
-									<img src="https://via.placeholder.com/322" alt="" />
-								</a>
-							</div>
-							<!-- small feature -->
-							<div class="col-6">
-								<a href="#">
-									<img src="https://via.placeholder.com/322" alt="" />
-								</a>
+				<div class="container">
+					<h3>ENJOY SUMMER FLAVORS</h3>
+					<div class="row">
+						<!-- feature left -->
+						<div class="col-6">
+							<a href="#" class="large-image">
+								<img src="https://via.placeholder.com/675" alt="" />
+							</a>
+						</div>
+						<!-- feature right -->
+						<div class="col-6">
+							<div class="row">
+								<!-- small feature -->
+								<div class="col-6">
+									<a href="#" class="small-image">
+										<img src="https://via.placeholder.com/322" alt="" />
+									</a>
+								</div>
+								<!-- small feature -->
+								<div class="col-6">
+									<a href="#" class="small-image">
+										<img src="https://via.placeholder.com/322" alt="" />
+									</a>
+								</div>
+								<!-- small feature -->
+								<div class="col-6">
+									<a href="#" class="small-image">
+										<img src="https://via.placeholder.com/322" alt="" />
+									</a>
+								</div>
+								<!-- small feature -->
+								<div class="col-6">
+									<a href="#" class="small-image">
+										<img src="https://via.placeholder.com/322" alt="" />
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="feature-banner">
-					<a href="#">
-						<img src="https://via.placeholder.com/1380x350" alt="" />
-					</a>
+					<div class="feature-banner">
+						<a href="#">
+							<img src="https://via.placeholder.com/1380x350" alt="" />
+						</a>
+					</div>
 				</div>
 			</section>
 			<!-- footer -->
 			<footer class="footer">
 				<div class="sitemap">
-					<div class="row">
-						<!-- footer 1 -->
-						<div class="col-3">
-							<h4>About Starbugs</h4>
-							<ul class="sitemap-link">
-								<li>Our Heritage</li>
-								<li>Our Company</li>
-								<li>Responsibility</li>
-								<li>Career Center</li>
-							</ul>
-						</div>
-						<!-- footer 2 -->
-						<div class="col-3">
-							<h4>Drink at Home</h4>
-							<ul class="sitemap-link">
-								<li>Coffee Story</li>
-								<li>Coffee & Tea at Home</li>
-								<li>How to Brew</li>
-							</ul>
-						</div>
-						<!-- footer 3 -->
-						<div class="col-3">
-							<ul class="sitemap-link">
-								<li>Starbugs rewards</li>
-								<li>Membership</li>
-								<li>More Information</li>
-								<li>Help Center</li>
-							</ul>
-						</div>
-						<!-- footer 4 -->
-						<div class="col-3">
-							<ul class="footer-social-links">
-								<li>
-									<a href="#">
-										<img src="https://via.placeholder.com/48" alt="facebook" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="https://via.placeholder.com/48" alt="twitter" />
-									</a>
-								</li>
-								<li>
-									<a href="#">
-										<img src="https://via.placeholder.com/48" alt="line" />
-									</a>
-								</li>
-							</ul>
-							<p>This is a demo website that built for WordPress theme development, Zero to Pro course.</p>
+					<div class="container">
+						<div class="row">
+							<!-- footer 1 -->
+							<div class="col-3">
+								<h4>About Starbugs</h4>
+								<ul class="sitemap-link">
+									<li><a href="#">Our Heritage</a></li>
+									<li><a href="#">Our Company</a></li>
+									<li><a href="#">Responsibility</a></li>
+									<li><a href="#">Career Center</a></li>
+								</ul>
+							</div>
+							<!-- footer 2 -->
+							<div class="col-3">
+								<h4>Drink at Home</h4>
+								<ul class="sitemap-link">
+									<li><a href="#">Coffee Story</a></li>
+									<li><a href="#">Coffee & Tea at Home</a></li>
+									<li><a href="#">How to Brew</a></li>
+								</ul>
+							</div>
+							<!-- footer 3 -->
+							<div class="col-3">
+								<ul class="sitemap-link">
+									<li><a href="#">Starbugs rewards</a></li>
+									<li><a href="#">Membership</a></li>
+									<li><a href="#">More Information</a></li>
+									<li><a href="#">Help Center</a></li>
+								</ul>
+							</div>
+							<!-- footer 4 -->
+							<div class="col-3">
+								<ul class="footer-social-links">
+									<li>
+										<a href="#">
+											<img src="https://via.placeholder.com/48" alt="facebook" />
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<img src="https://via.placeholder.com/48" alt="twitter" />
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<img src="https://via.placeholder.com/48" alt="line" />
+										</a>
+									</li>
+								</ul>
+								<p>This is a demo website that built for WordPress theme development, Zero to Pro course.</p>
+							</div>
 						</div>
 					</div>
 				</div>
 				<div class="copyright">
-					<div class="row">
-						<!-- copyright 1 -->
-						<div class="col-6">
-							<ul class="copyright-link">
-								<li><a href="#">Homepage</a></li>
-								<li><a href="#">Menus</a></li>
-								<li><a href="#">Promotions</a></li>
-								<li><a href="#">Contact Us</a></li>
-							</ul>
-						</div>
-						<!-- copyright 2 -->
-						<div class="col-6">
-							<p>Starbugs.com | WordPress Theme for Zero to Pro Course.</p>
+					<div class="container">
+						<div class="row">
+							<!-- copyright 1 -->
+							<div class="col-6">
+								<ul class="copyright-link">
+									<li><a href="#">Homepage</a></li>
+									<li><a href="#">Menus</a></li>
+									<li><a href="#">Promotions</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+							</div>
+							<!-- copyright 2 -->
+							<div class="col-6">
+								<p class="copyright-text">Starbugs.com | WordPress Theme for Zero to Pro Course.</p>
+							</div>
 						</div>
 					</div>
 				</div>
