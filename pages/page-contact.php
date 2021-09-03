780064d808f6075c7e6401c9794cfc0437a383c9
Jirayu Limjinda <jir4yu.l@gmail.com>
2021-09-03 10:30:24 +0700

    add theloop to contact

--- pages/page-contact.php
+++ pages/page-contact.php
@@ -17,11 +17,11 @@
   <section class="contact-content">
     <article class="page-content">
       <div class="container">
-        <p>We love hearing from our customers, all questions, comments and feedback are always welcome. They help us to ensure that every Starbugs experience you have is the best it can be.</p>
-        <h4>Customer Care</h4>
-        <p>You can call us at +66 0000 0000 from 9.00 a.m. – 6.00 p.m. Monday to Sunday. Alternatively, you can send us an email to customer@coffeecoffee.co.th, and we’ll do our best to reply as quickly as possible.</p>
-        <h4>Starbucks Thailand Headquarter</h4>
-        <p>You can call us at +66 0808 0000 from 9.00 a.m. – 6.00 p.m. Monday to Sunday. Alternatively, you can send us an email to customer@coffee-hq.th, and we’ll do our best to reply as quickly as possible.</p>
+        <?php if (have_posts()) : ?>
+          <?php while(have_posts()) : the_post(); ?>
+            <?php the_content(); ?>
+          <?php endwhile; ?>
+        <?php endif; ?>
       </div>
     </article>
   </section>
@@ -32,7 +32,6 @@
       <h3>Contact Form</h3>
       <p>We will get back to you as soon as possible</p>
       <?php echo do_shortcode('[contact-form-7 id="60" title="Contact Us Form"]') ?>
-      
     </div>
   </section>
 </div>
