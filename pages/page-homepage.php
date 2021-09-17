3b5ba6a65015051d8a493f0ab7382ed565bcbb6b
Jirayu Limjinda <jir4yu.l@gmail.com>
2021-09-18 00:23:06 +0700

    wp query for homepage and promotion

--- pages/page-homepage.php
+++ pages/page-homepage.php
@@ -1,4 +1,19 @@
-<?php /* Template Name: Homepage */ ?>
+<?php 
+	/* Template Name: Homepage */ 
+	$top_featured = new WP_Query( array(
+		'category_name' => 'Featured',
+		'posts_per_page' => 1,
+		'orderby' => 'date',
+		'order' => 'ASC'
+	));
+	$other_featured = new WP_Query( array(
+		'category_name' => 'Featured',
+		'posts_per_page' => 4,
+		'offset' => 1,
+		'orderby' => 'date',
+		'order' => 'ASC'
+	));
+?>
 
 <?php get_header(); ?>
 	<?php get_template_part('views/slider'); ?>
@@ -8,39 +23,28 @@
 			<h3>ENJOY SUMMER FLAVORS</h3>
 			<div class="row">
 				<!-- feature left -->
-				<div class="col-md-6">
-					<a href="#" class="large-image">
-						<img src="<?php echo get_template_directory_uri() ?>/img/large.jpg" alt="feature 1" />
-					</a>
-				</div>
+				<?php if ( $top_featured->have_posts()) : ?>
+					<?php while ( $top_featured->have_posts()) : $top_featured->the_post(); ?>
+						<div class="col-md-6">
+							<a href="<?php the_permalink(); ?>" class="large-image" title="<?php the_title(); ?>">
+								<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php the_title(); ?>" />
+							</a>
+						</div>
+					<?php endwhile; ?>	
+				<?php endif; ?>
 				<!-- feature right -->
 				<div class="col-md-6">
-					<div class="row">
-						<!-- small feature -->
-						<div class="col-6">
-							<a href="#" class="small-image">
-								<img src="<?php echo get_template_directory_uri() ?>/img/small1.jpg" alt="Small feature" />
-							</a>
-						</div>
-						<!-- small feature -->
-						<div class="col-6">
-							<a href="#" class="small-image">
-								<img src="<?php echo get_template_directory_uri() ?>/img/small2.jpg" alt="Small feature" />
-							</a>
-						</div>
-						<!-- small feature -->
-						<div class="col-6">
-							<a href="#" class="small-image">
-								<img src="<?php echo get_template_directory_uri() ?>/img/small3.jpg" alt="Small feature" />
-							</a>
-						</div>
-						<!-- small feature -->
-						<div class="col-6">
-							<a href="#" class="small-image">
-								<img src="<?php echo get_template_directory_uri() ?>/img/small4.jpg" alt="Small feature" />
-							</a>
-						</div>
-					</div>
+					<?php if ( $other_featured->have_posts()) : ?>
+						<div class="row">
+							<?php while ( $other_featured->have_posts()) : $other_featured->the_post(); ?>
+								<div class="col-6">
+									<a href="<?php the_permalink(); ?>" class="small-image" title="<?php the_title(); ?>">
+										<img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'product' ) ?>" alt="<?php the_title(); ?>" />
+									</a>
+								</div>
+							<?php endwhile; ?>
+						</div>	
+					<?php endif; ?>
 				</div>
 			</div>
 			<div class="feature-banner">
--- pages/page-promotion.php
+++ pages/page-promotion.php
@@ -1,4 +1,12 @@
-<?php /* Template Name: Promotion */ ?>
+<?php 
+  /* Template Name: Promotion */ 
+  $promotions = new WP_Query( array(
+    'category_name' => 'Promotion',
+    'posts_per_page' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC',
+  ));
+?>
 
 <?php get_header(); ?>
 
@@ -6,20 +14,16 @@
   <div class="container">
     <!-- promotion banner -->
     <section class="main-promotion-banner">
-      <img src="<?php echo get_template_directory_uri()?>/img/main-promotion.jpg" alt="" />
+      <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full') ?>" alt="<?php the_title()?>" />
     </section>
     <!-- content -->
     <section class="main-promotion-content">
       <article class="page-content">
-        <h3>Summer Jelly Bag Set</h3>
-        <p>
-          User experience hypotheses series A financing supply chain pivot mass market leverage conversion prototype. Business-to-consumer startup niche market long tail iteration agile development direct mailing. Handshake A/B testing termsheet marketing pivot business model canvas. Twitter virality low hanging fruit MVP founders.
-        </p>
-        <ul>
-          <li>Marketing graphical user interface stock interaction design.</li>
-          <li>Low hanging fruit A/B testing gen-z branding business plan burn rate termsheet business-to-business bootstrapping.</li>
-          <li>Advisor hackathon burn rate accelerator venture incubator.</li>
-        </ul>
+        <?php if (have_posts()): ?>
+          <?php while(have_posts()): the_post();?>
+            <?php the_content(); ?>
+          <?php endwhile; ?>  
+        <?php endif; ?>
       </article>
     </section>
     <!-- share -->
@@ -46,35 +50,20 @@
     <section class="main-promotion-related">
       <div class="row">
         <!-- banner1 -->
-        <div class="col-md-4">
-          <div class="related-item">
-            <a href="#">
-              <img src="<?php echo get_template_directory_uri()?>/img/promotion-banner1.jpg" alt="" />
-            </a>
-            <h4><a href="#">Buy 1 Get 1 Free</a></h4>
-            <p>12/07/2021</p>
-          </div>
-        </div>
-        <!-- banner2 -->
-        <div class="col-md-4">
-          <div class="related-item">
-            <a href="#">
-              <img src="<?php echo get_template_directory_uri()?>/img/promotion-banner2.jpg" alt="" />
-            </a>
-            <h4><a href="#">Earn 23 Bonus Stars</a></h4>
-            <p>10/07/2021</p>
-          </div>
-        </div>
-        <!-- banner 3 -->
-        <div class="col-md-4">
-          <div class="related-item">
-            <a href="#">
-              <img src="<?php echo get_template_directory_uri()?>/img/promotion-banner3.jpg" alt="" />
-            </a>
-            <h4><a href="#">Choux Cream Dreamland</a></h4>
-            <p>02/07/2021</p>
-          </div>
-        </div>
+        <?php if ($promotions->have_posts()) : ?>
+          <?php while ($promotions->have_posts()): $promotions->the_post(); ?>
+            <div class="col-md-4">
+              <div class="related-item">
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                  <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" />
+                </a>
+                <h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
+                <p><?php echo get_the_date(); ?></p>
+              </div>
+            </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+
       </div>
     </section>
   </div>
